582a675288654238811fb913a3407fb211271500
nagataku26 <nagataku.biz@gmail.com>
2024-04-06 07:26:24 +0900

    pla33

--- Pla31.java
+++ Pla31.java
@@ -1,4 +1,4 @@
-public class practice31 {
+public class Pla31 {
   weight == 60
   (age1 + age2) * 2 > 60
   age % 2 == 1
---
+++ Pla33.java
@@ -0,0 +1,16 @@
+public class Pla33 {
+  public static void main(String[] args) {
+    int isHungry = 0;
+    String food = "ハンバーグ";
+    System.out.println("こんにちは");
+    if (isHungry == 0){
+      System.out.println("お腹がいっぱいです");
+    } else {
+      System.out.println("はらぺこです");
+    }
+    if (isHungry == 1) {
+      System.out.println(food + "をいただきます");
+    }
+    System.out.println("ごちそうさまでした");
+  }
+}
