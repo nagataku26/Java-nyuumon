3fd22ac79fd0f57ae44c449db940bf69a4ac4e87
nagataku26 <nagataku.biz@gmail.com>
2024-04-05 13:35:46 +0900

    32

---
+++ Code32.java
@@ -0,0 +1,9 @@
+public class Code32 {
+  public static void main(String[] args) {
+    boolean doorClose = true; //ここでtrueかfalseを代入
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }
+  }
+}
