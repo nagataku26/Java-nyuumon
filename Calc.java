d2c3df9a4c315763ab144ed7f79942b9a701beb2
nagataku26 <nagataku.biz@gmail.com>
2024-04-09 16:01:23 +0900

    calc

--- Calc.java
+++ Calc.java
@@ -1,4 +1,7 @@
-public class Code61 {
+package calcapp.main;
+import calcapp.logics.CalcLogic;
+
+public class Calc {
   public static void main(String[] args) {
     int a = 10; int b = 2;
     int total = CalcLogic.tasu(a, b);
