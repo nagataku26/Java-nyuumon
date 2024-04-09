dfd90913bbef83cc57c02c67fb824a5c0ba2cf16
nagataku26 <nagataku.biz@gmail.com>
2024-04-09 10:21:47 +0900

    Pla52

---
+++ Pla52.java
@@ -0,0 +1,13 @@
+public class Pla52 {
+  public static void main(String[] args) {
+    String address = "メールの宛先アドレス";
+    String title = "メールのタイトル";
+    String text = "メールの本文";
+    email(address, title, text);
+  }
+  public static void email(String address, String title, String text) {
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：" + title);
+    System.out.println("本文：" + text);
+  }
+}
