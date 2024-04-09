1c30e55b10a0c7f5870f0244aa6775fe9595d41d
nagataku26 <nagataku.biz@gmail.com>
2024-04-09 10:03:22 +0900

    pla51

---
+++ Pla51.java
@@ -0,0 +1,12 @@
+public class Pla51 {
+  public static void introduceOneself() {
+    String name = "湊雄輔";
+    int age = 22;
+    double height = 169.9;
+    char zodiac = '辰';
+    System.out.println("私の名前は" + name + "です。歳は" + age +"歳です。身長は" + height + "cmです。十二支は" + zodiac + "です。");
+  }
+  public static void main(String[] args) {
+    introduceOneself();
+  }
+}
