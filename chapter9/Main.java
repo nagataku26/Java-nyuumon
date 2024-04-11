dda959b4e92057af6e67fca9793fcb191ccb23eb
nagataku26 <nagataku.biz@gmail.com>
2024-04-11 14:12:58 +0900

    912

--- chapter9/Main.java
+++ chapter9/Main.java
@@ -1,16 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    Hero h1 = new Hero();
-    h1.name = "ミナト";
-    h1.hp = 100;
-    Hero h2 = new Hero();
-    h2.name = "アサカ";
-    h2.hp = 100;
+    Hero h1 = new Hero("ミナト");
+    Hero h2 = new Hero("アサカ");
     Wizard w = new Wizard();
     w.name = "スガワラ";
     w.hp = 50;
     w.heal(h1);
     w.heal(h2);
     w.heal(h2);
+    System.out.println(h1.name);
+    System.out.println(h2.name);
+    System.out.println(h2.hp);
   }
 }
