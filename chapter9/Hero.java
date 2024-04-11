c9bc86591960584ab126ffac46f70db3eb9b02ea
nagataku26 <nagataku.biz@gmail.com>
2024-04-11 12:57:35 +0900

    98

--- chapter9/Hero.java
+++ chapter9/Hero.java
@@ -6,4 +6,7 @@ public class Hero {
     System.out.println(this.name + "は" + this.sword.name + "で攻撃した！");
     System.out.println("敵に5ポイントのダメージをあたえた！");
   }
+  public Hero() {
+    this.hp = 100;
+  }
 }
