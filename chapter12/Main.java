c3214633c114aaa7cea427a6ca0b2c7c9ee618f6
nagataku26 <nagataku.biz@gmail.com>
2024-04-15 14:25:27 +0900

    12

--- chapter12/Main.java
+++ chapter12/Main.java
@@ -1,9 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    Wizard w = new Wizard();
-    Character c = w;
-    c.name = "アサカ";
-    c.attack();
-    c.fireball();
+    Character[] c = new Character[5];
+    c[0] = new Hero();
+    c[1] = new Hero();
+    c[2] = new Thief();
+    c[3] = new Wizard();
+    c[4] = new Wizard();
+
+    for (Character ch : c) {
+      ch.hp += 50;
+    }
   }
 }
