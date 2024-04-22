c6d21c3e9d05fe12a096fd57317b5be23ba2c3a1
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 15:28:42 +0900

    1514

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,13 +1,23 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
-    SimpleDateFormat f =new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-    Date d = f.parse("2023/09/18 05:53:20");
-    System.out.println(d);
-    Date now = new Date();
-    String s = f.format(now);
-    System.out.println("現在は" + s + "です");
+  public static void main(String[] args) {
+    Instant i1 = Instant.now();
+    Instant i2 = Instant.ofEpochMilli(1600705425827L);
+    long l = i2.toEpochMilli();
+
+    ZonedDateTime zi = ZonedDateTime.now();
+    ZonedDateTime z2 = ZonedDateTime.of(2023, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
+
+    Instant i3 = z2.toInstant();
+    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+
+    System.out.println("東京：" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+
+    System.out.println("ロンドン：" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
+
+    if (z2.isEqual(z3)) {
+      System.out.println("これらは同じ瞬間を指しています");
+    }
   }
 }
