5980d2c967810d01c813b63b600ac2836be6c9e2
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 14:58:44 +0900

    1513

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,14 +1,13 @@
-import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
-  public static void main(String[] args) {
-    Calendar c = Calendar.getInstance();
-    c.set(2023, 8, 18, 5, 53, 20);
-    c.set(Calendar.MONTH, 9);
-    Date d = c.getTime();
+  public static void main(String[] args) throws Exception {
+    SimpleDateFormat f =new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    Date d = f.parse("2023/09/18 05:53:20");
     System.out.println(d);
     Date now = new Date();
-    c.setTime(now);
-    int y = c.get(Calendar.YEAR);
-    System.out.println("今年は" + y + "年です");
+    String s = f.format(now);
+    System.out.println("現在は" + s + "です");
   }
 }
