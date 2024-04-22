a7dfe415d68ca1acc2df8acfcc9b29838135cc3e
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 15:57:36 +0900

    1516

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,12 +1,18 @@
 import java.time.*;
+import java.time.format.*;
 
 public class Main {
   public static void main(String[] args) {
-    LocalDateTime l1 = LocalDateTime.now();
-    LocalDateTime l2 = LocalDateTime.of(2024, 1, 1, 9, 5, 0, 0);
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDate ldate = LocalDate.parse("2023/09/22", fmt);
 
-    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
-    LocalDateTime l3 = z1.toLocalDateTime();
-    System.out.println(l3);
+    LocalDate ldatep = ldate.plusDays(1000);
+    String str = ldatep.format(fmt);
+    System.out.println("1000日後は" + str);
+
+    LocalDate now = LocalDate.now();
+    if (now.isBefore(ldatep)) {
+      System.out.println("本日は、その日より過去です");
+    }
   }
 }
