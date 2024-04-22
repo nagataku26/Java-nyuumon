56f62ee01e0717db0cff768cb81c6f0c5bc6565d
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 15:39:19 +0900

    1515

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -2,22 +2,11 @@ import java.time.*;
 
 public class Main {
   public static void main(String[] args) {
-    Instant i1 = Instant.now();
-    Instant i2 = Instant.ofEpochMilli(1600705425827L);
-    long l = i2.toEpochMilli();
+    LocalDateTime l1 = LocalDateTime.now();
+    LocalDateTime l2 = LocalDateTime.of(2024, 1, 1, 9, 5, 0, 0);
 
-    ZonedDateTime zi = ZonedDateTime.now();
-    ZonedDateTime z2 = ZonedDateTime.of(2023, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
-
-    Instant i3 = z2.toInstant();
-    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-
-    System.out.println("東京：" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
-
-    System.out.println("ロンドン：" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
-
-    if (z2.isEqual(z3)) {
-      System.out.println("これらは同じ瞬間を指しています");
-    }
+    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+    LocalDateTime l3 = z1.toLocalDateTime();
+    System.out.println(l3);
   }
 }
