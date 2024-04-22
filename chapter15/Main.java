2ee25a5cd27df700036dbc7b1a5ae80373fe7a25
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 14:18:00 +0900

    1511

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,7 +1,10 @@
+import java.util.Date;
 public class Main {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    long end = System.currentTimeMillis();
-    System.out.println("処理にかかった時間は・・・" + (end - start) + "ミリ秒でした");
-  }
+    Date now = new Date();
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(1694984000000L);
+    System.out.println(past);
+    }
 }
