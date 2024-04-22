16e1666ed6fc4df247462267d51bfa8b63068343
nagataku26 <nagataku.biz@gmail.com>
2024-04-22 14:30:44 +0900

    1512

--- chapter15/Main.java
+++ chapter15/Main.java
@@ -1,10 +1,14 @@
-import java.util.Date;
+import java.util.*;
 public class Main {
   public static void main(String[] args) {
+    Calendar c = Calendar.getInstance();
+    c.set(2023, 8, 18, 5, 53, 20);
+    c.set(Calendar.MONTH, 9);
+    Date d = c.getTime();
+    System.out.println(d);
     Date now = new Date();
-    System.out.println(now);
-    System.out.println(now.getTime());
-    Date past = new Date(1694984000000L);
-    System.out.println(past);
-    }
+    c.setTime(now);
+    int y = c.get(Calendar.YEAR);
+    System.out.println("今年は" + y + "年です");
+  }
 }
