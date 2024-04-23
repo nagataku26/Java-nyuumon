b4728d3520e651bebdfec4bfed71533f046c76af
nagataku26 <nagataku.biz@gmail.com>
2024-04-23 22:59:47 +0900

    163

--- chapter16/Main.java
+++ chapter16/Main.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Map<String, Integer> prefs = new HashMap<String, Integer>();
-    prefs.put("京都府", 255);
-    prefs.put("東京都", 1261);
-    prefs.put("熊本県",181);
-    for (String key : prefs.keySet()) {
-      int value = prefs.get(key);
-      System.out.println(key + "の人口は" + value);
+    Hero h1 = new Hero("斉藤");
+    Hero h2 = new Hero("鈴木");
+    Map<Hero, Integer> heroes = new HashMap<Hero, Integer>();
+    heroes.put(h1, 3);
+    heroes.put(h2, 7);
+    for (Hero key : heroes.keySet()) {
+      int value = heroes.get(key);
+      System.out.println(key.getName() + "が倒した敵＝" + value);
     }
   }
 }
