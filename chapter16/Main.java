559771cdda0a30c61c40f1d882542ee1ff48e213
nagataku26 <nagataku.biz@gmail.com>
2024-04-23 14:54:05 +0900

    164

--- chapter16/Main.java
+++ chapter16/Main.java
@@ -1,13 +1,15 @@
-import java.util.HashSet;
+import java.util.TreeSet;
 import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
-    Set<String> colors = new HashSet<String>();
-    colors.add("赤");
-    colors.add("青");
-    colors.add("黄");
-    colors.add("赤");
-    System.out.println("色は" + colors.size() + "種類です");
+    Set<String> words = new TreeSet<String>();
+    words.add("dog");
+    words.add("cat");
+    words.add("wolf");
+    words.add("panda");
+    for (String s : words) {
+      System.out.print(s + "→");
+    }
   }
 }
