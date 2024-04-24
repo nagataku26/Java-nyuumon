7a2d24d5ab3f934389b583e694866bf5c6f1d2c1
nagataku26 <nagataku.biz@gmail.com>
2024-04-24 22:03:59 +0900

    173

--- chapter17/Main.java
+++ chapter17/Main.java
@@ -2,12 +2,7 @@ import java.io.*;
 
 public class Main {
   public static void main(String[] args) {
-    try {
-      FileWriter fw = new FileWriter("data.txt");
-      fw.write("hello!");
-      fw.close();
-    } catch (Exception e) {
-      System.out.println("何らかの例外が発生しました");
-    }
+    Person p = new Person();
+    p.setAge(-128);
   }
 }
